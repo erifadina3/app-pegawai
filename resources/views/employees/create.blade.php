eef1bfd30e26f97a6ee08e070ec97b1a2ef1d031
Erifadina <nlegiaerifadina@gmail.com>
2025-10-01 10:01:14 +0700

    Penambahan Master Layout

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -16,36 +16,36 @@
                <td><label for="email">Email:</label></td>
                <td><input type="email" id="email" name="email"></td>
            </tr>
-<tr>
-<td><label for="nomor_telepon">Nomor Telepon:</label></td>
-<td><input type="text" id="nomor_telepon" name="nomor_telepon"></td>
-</tr>
-<tr>
-<td><label for="tanggal_lahir">Tanggal Lahir:</label></td>
-<td><input type="date" id="tanggal_lahir" name="tanggal_lahir"></td>
-</tr>
-<tr>
-<td><label for="alamat">Alamat:</label></td>
-<td><textarea id="alamat" name="alamat"></textarea></td>
-</tr>
-<tr>
-<td><label for="tanggal_masuk">Tanggal Masuk:</label></td>
-<td><input type="date" id="tanggal_masuk" name="tanggal_masuk"></td>
-</tr>
-<tr>
-<td><label for="status">Status:</label></td>
-<td>
-<select id="status" name="status">
-<option value="aktif">Aktif</option>
-<option value="nonaktif">Nonaktif</option>
-</select>
-</td>
-</tr>
-<tr>
-<td colspan="2" style="text-align:right;">
-<button type="submit">Simpan</button>
-</td>
-</tr>
-</table>
+           <tr>
+               <td><label for="nomor_telepon">Nomor Telepon:</label></td>
+               <td><input type="text" id="nomor_telepon" name="nomor_telepon"></td>
+           </tr>
+           <tr> 
+               <td><label for="tanggal_lahir">Tanggal Lahir:</label></td>
+               <td><input type="date" id="tanggal_lahir" name="tanggal_lahir"></td>
+          </tr>
+          <tr>
+               <td><label for="alamat">Alamat:</label></td>
+               <td><textarea id="alamat" name="alamat"></textarea></td>
+          </tr>
+          <tr>
+               <td><label for="tanggal_masuk">Tanggal Masuk:</label></td>
+               <td><input type="date" id="tanggal_masuk" name="tanggal_masuk"></td>
+          </tr>
+          <tr>
+               <td><label for="status">Status:</label></td>
+               <td> 
+                    <select id="status" name="status">
+                    <option value="aktif">Aktif</option>
+                    <option value="nonaktif">Nonaktif</option>
+                  </select>
+               </td>
+         </tr>
+         <tr>
+              <td colspan="2" style="text-align:right;">
+                <button type="submit">Simpan</button>
+              </td>
+         </tr>
+        </table>
 </body>
 </html>
